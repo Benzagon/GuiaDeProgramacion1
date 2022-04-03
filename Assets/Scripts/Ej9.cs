531bc747c09ad9d89c00c77a483bf68d19c20686
Benzagon <gonbenzaquen@gmail.com>
2022-04-03 20:51:11 -0300

    Ej7/8

---
+++ Assets/Scripts/Ej9.cs
@@ -0,0 +1,23 @@
+﻿//9. Realizá un programa que resuelva el siguiente problema:
+//Tres personas aportan diferente capital a una sociedad.Se desea saber qué porcentaje del
+//total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado
+//por las tres.
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego
+//mostrar lo pedido en el siguiente formato:
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ ….
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej9 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
