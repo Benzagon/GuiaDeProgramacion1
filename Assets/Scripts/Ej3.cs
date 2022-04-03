1a97e7b3a216f7b68ac2a8b27273c1fcfcebc7e0
Benzagon <gonbenzaquen@gmail.com>
2022-04-03 17:56:33 -0300

    Ej2 - Ej6

--- Assets/Scripts/Ej3.cs
+++ Assets/Scripts/Ej3.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿//3. Crear una variable pública que permita ingresar texto.Permitir que al ejecutar el programa
+//se muestre en la consola un mensaje de saludo que incluya el texto de la variable.Por
+//ejemplo, si en la variable se ingresa el texto “Ana”, el mensaje en consola debe decir “Hola
+//Ana”.
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ej3 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string Name;
+
     void Start()
     {
-        
+        Debug.Log("Hola " + Name);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
