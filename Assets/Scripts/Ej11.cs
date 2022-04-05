15c620eb01a8a13145f1c3807f8d51a703fb97fb
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-04-05 09:06:18 -0300

    Ej9-10

---
+++ Assets/Scripts/Ej11.cs
@@ -0,0 +1,35 @@
+﻿//11. Realizá un programa que resuelva el siguiente problema:
+//Deberá ingresarse por Inspector una fecha de compra(String en el siguiente formato:
+//YYYYMMDD), un nombre de comprador, un nombre de producto y una cantidad y precio del
+//producto comprado.Mostrá a modo de ticket, la información ingresada y el monto a pagar.
+//Modelo de Ticket:
+//Fecha de Compra: YYYYMMDD
+//Nombre del Comprador: xxxxx xxxxx
+//Producto solicitado: xxxxx
+//Cantidad solicitada: xx
+//Precio Unitario: $xxx
+//Total a Pagar: $xxxxx
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej11 : MonoBehaviour
+{
+    public string fechaCompra, nombreComprador, nombreProducto;
+    public int cantProducto;
+    public float precioProducto;
+        
+    void Start()
+    {
+        float precioTotal = precioProducto * cantProducto;
+
+        Debug.Log("Fecha de Compra: " + fechaCompra + "" +
+            "Nombre del Comprador: " + nombreComprador);
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Ej9.cs
+++ Assets/Scripts/Ej9.cs
@@ -11,9 +11,17 @@ using UnityEngine;
 
 public class Ej9 : MonoBehaviour
 {
+    public float cap1, cap2, cap3;
+    
+    public string name1, name2, name3;
+
     void Start()
     {
-        
+        float capTotal = cap1 + cap2 + cap3;
+
+        Debug.Log("Nombre: " + name1 + ", Capital aportado: $" + cap1 + ", Porcentaje del capital: " + ((cap1 / capTotal) * 100) + "%, Monto total aportado: $" + capTotal);
+        Debug.Log("Nombre: " + name2 + ", Capital aportado: $" + cap2 + ", Porcentaje del capital: " + ((cap2 / capTotal) * 100) + "%, Monto total aportado: $" + capTotal);
+        Debug.Log("Nombre: " + name3 + ", Capital aportado: $" + cap3 + ", Porcentaje del capital: " + ((cap3 / capTotal) * 100) + "%, Monto total aportado: $" + capTotal);
     }
 
     void Update()
